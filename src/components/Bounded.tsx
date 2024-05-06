03dcfc97d5f82d78988c768f588ec8952c4f5259
friessssss <zach@zachtrobertson.comm>
2024-05-06 17:22:50 -0400

    fixed width

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -16,7 +16,7 @@ const Bounded = React.forwardRef<HTMLDivElement, BoundedProps>(
                 className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
                 {...restProps}
             >
-                <div className="mx-auto w-full max-w-7xl">
+                <div className="mx-auto w-full">
                 {children}
                 </div>
             </Comp>
