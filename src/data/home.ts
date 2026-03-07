792dfca79bb902d463071d739f068d91d906c9b1
friessssss <zach@zachtrobertson.comm>
2026-03-07 15:48:47 -0700

    typo

--- src/data/home.ts
+++ src/data/home.ts
@@ -45,7 +45,7 @@ export const workHistory = [
     year: '2024 - Present',
     title: 'Hardware Software Integration Engineer',
     company: 'Lightship',
-    description: 'Developed complete diagnostic tooling for Lightship\'s fleet of vehicles. Included a custom Linux kernel, CAN bus drivers, and a web-based diagnostic interface. Learned Ins and Outs of the complete vehicle stack in order to effective troubleshoot and repair vehicles.',
+    description: 'Developed complete diagnostic tooling for Lightship\'s fleet of vehicles. Included a custom Linux kernel, CAN bus drivers, and a web-based diagnostic interface. Learned Ins and Outs of the complete vehicle stack in order to effectively troubleshoot and repair vehicles.',
     technologies: ['Python', 'Linux Development', 'CAN', 'Diagnostics', 'Jack of all trades'],
   },
   {
